f93877c7c7e4e4083721bcbd57361b41b9c9b63b
mamta srinivas <mamtasrinivassp@gmail.com>
2020-05-06 15:24:53 +0200

    Setup

---
+++ server.js
@@ -0,0 +1,34 @@
+require('./connect')
+
+const express = require('express')
+const app = express()
+
+const cors = require('cors');
+app.use(cors());
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+// app.use(express.urlencoded());
+
+//Get all information in JSON
+app.use((res, req, next)=>{
+    res.setHeader('Content-type', 'application/json')
+    next()
+})
+
+//Home Route
+app.get('/', (req, res) => res.send('Hello World!'))
+
+//All database endpoints
+//app.use('/messages', messages)
+
+
+
+app.use((error, req, res, next)=>{
+    console.log(error)
+    return res.status(error.statusCode).send({errorMessage: error.message})
+})
+
+app.listen(port, () => console.log(`Hello World app listening at http://localhost:${port}`))
