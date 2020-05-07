1ea9423b03dd50929283c1ee31e37fc194388105
mamta srinivas <mamtasrinivassp@gmail.com>
2020-05-07 21:13:40 +0200

    Read and Update

--- routers/stories.js
+++ routers/stories.js
@@ -5,14 +5,45 @@ const StoryModel = require('../models/storiesModel')
 const router = express.Router()
 
 //Create a Story
-router.post('/', async(req, res) => {
-    console.log(req.body)
-    const story = req.body
-    const storyInfo = await new StoryModel(story)
-    const storySave = await storyInfo.save()
-    console.log(storySave)
-    res.send(JSON.stringify(storySave))
+router.post('/', async (req, res) => {
+    try {
+        console.log(req.body)
+        const story = req.body
+        const storyInfo = await new StoryModel(story)
+        const storySave = await storyInfo.save()
+        console.log(storySave)
+        res.send(JSON.stringify(storySave))
+    }
+    catch (error) {
+        console.log(error);
+    }
 })
 
-//
+//Read all Stories
+router.get('/', (req, res) => {
+        console.log(req.body)
+        StoryModel.find((err, allStories) => {
+            if (err) return res.status(500).send(err)
+            return res.status(200).send(allStories);
+        })   
+})
+
+//Update specific story
+router.put('/', (req, res) => {
+        const { id } = req.body
+        StoryModel.findByIdAndUpdate(id, req.body, { new: true, useFindAndModify: false }, (err, story) => {
+            if (err) return res.status(500).send(err);
+            return res.status(200).send(story)
+        })
+})
+
+//Delete all Stories
+router.delete('/', async (req, res) => {
+    try {
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
 module.exports = router;
