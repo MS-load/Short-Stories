bb63a807bc01870aaefa1402fe9981d01d3efd44
Kristóf <krstf456@gmail.com>
2020-05-08 13:18:23 +0200

    connect.js works

--- server.js
+++ server.js
@@ -3,62 +3,51 @@ require('./connect')
 
 const express = require('express')
 const cors = require('cors')
-const bodyParser = require('body-parser')
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(cors())
 app.use(
-    bodyParser.urlencoded({
+    express.urlencoded({
         extended: false
     })
 )
 
-const mongoURI = 'mongodb://localhost:27017/users'
-
-mongoose
-    .connect(mongoURI, {useNewUrlParser: true})
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err))
-
+const stories = require('./routers/stories')
 const Users = require('./routers/Users')
 
+//Home Route
+app.get('/', (req, res) => res.send('Hello World!'))
+
+//All database endpoints
+app.use('/stories', stories)
 app.use('/users', Users)
 
-app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
+app.use((error, req, res, next)=>{
+    console.log(error)
+    return res.status(error.statusCode).send({errorMessage: error.message})
 })
 
+app.listen(port, () => console.log(`Hello World app listening at http://localhost:${port}`))
 
 
-// const express = require('express')
-// const app = express()
 
-// const stories = require('./routers/stories')
 
-// // const cors = require('cors');
-// // app.use(cors());
-// app.use(express.json())
 
-// const port = process.env.PORT || 3000
+ 
 
-// // Parse URL-encoded bodies (as sent by HTML forms)
-// // app.use(express.urlencoded());
 
-// //Home Route
-// app.get('/', (req, res) => res.send('Hello World!'))
 
-// //All database endpoints
-// app.use('/stories', stories)
 
-// app.use((error, req, res, next)=>{
-//     console.log(error)
-//     return res.status(error.statusCode).send({errorMessage: error.message})
-// })
 
-// app.listen(port, () => console.log(`Hello World app listening at http://localhost:${port}`))
+
+ 
+
+
+
+ 
+
+
