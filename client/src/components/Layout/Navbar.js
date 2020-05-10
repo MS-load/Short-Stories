b2f060ab966d0e2a5d098a5dd2ecffc0a98ad4a3
Fluffinator97 <philip.arvidsson97@gmail.com>
2020-05-10 11:09:28 +0200

    Made the button better

--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => (
     <div>
         <header className="navbar">
             <div className="placeholderIcon"></div>
-            <div className="name">
-                
+            <div className="nameBox">
+                <h1 className="name"><Link to="/" className="linkStyle">Woofer</Link></h1>
             </div>
             <div className="registration">
-                <button className="button registerbutton">Register?</button>
+            <Link to="/register" className="linkStyle"><button className="button registerbutton">Register</button></Link>
             </div>
             <div className="login">
-                <button className="button loginbutton">Log in</button>
+            <Link to="/login" className="linkStyle"><button className="button loginbutton">login</button></Link>
             </div>
         </header>
     </div>
