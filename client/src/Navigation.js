9927e54ec8c068096142623ca314d7c329c45ab4
Fluffinator97 <philip.arvidsson97@gmail.com>
2020-05-11 09:52:25 +0200

    Missed an error

--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -15,8 +15,30 @@ export default class Navigation extends React.Component {
  
   render(){
     return (
+      <Navbar collapseOnSelect expand="lg" variant="dark" fixed="top" style={{ background: '#88BDBC' }}>
+        <Navbar.Brand>Short Stories</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
 
-
+            <Nav.Link onClick={() => this.setState({ modal: true})}
+            style={{ visibility: this.props.currentUser !== 'guest' ? 'show' : 'hidden' }}>
+            Add a story </Nav.Link>
+            <Nav.Link  style={{ visibility: this.props.currentUser !== 'guest' ? 'show' : 'hidden' }}>My Stories</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link>Login</Nav.Link>
+            <Nav.Link >Register</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <CenteredModal
+          show={this.state.modal}
+          onHide={() => this.setState({ modal: false })}
+          addAuthor={this.props.currentUser}
+          submitForm={this.props.addStory}
+          operation='add'
+        />
+      </Navbar>
     );
   }
 }
