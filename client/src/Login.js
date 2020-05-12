0952cd32c9afb37711708b5dc3374657b2fc58d0
Kristóf <krstf456@gmail.com>
2020-05-12 12:52:15 +0200

    basicLoginSetup

--- client/src/Login.js
+++ client/src/Login.js
@@ -9,11 +9,32 @@ class Login extends Component {
             password: '',
         
         }
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    onChange(e) {
+        this.setState({  [e.target.name]: e.target.value })
+
+    }
+    onSubmit(e) {
+        e.preventDefault()
+        
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        login(user).then(res => {
+            if (res) {
+                //this.props.history.push('/profile')
+            }
+        })
     }
 
     render() {
         return(
-            <div></div>
+            <div className='container'>
+                
+            </div>
         )
     }
 }
